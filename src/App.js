a7331d20eacb4d179cc38bce1b27d9271d4ccc09
MinhPhu1999 <minhphuson1999@gmail.com>
2023-04-14 09:00:48 +0700

    commit code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [number, setNumber] = useState(1);
+	const [color, setColor] = useState('');
+
+	const randomNumber = () => {
+		setColor('');
+		const random = Math.floor(Math.random() * 200);
+		setNumber(random);
+	};
+
+	const pickColor = async () => {
+		const eyeDropper = new window.EyeDropper();
+		const { sRGBHex } = await eyeDropper.open();
+		setColor(sRGBHex);
+	};
+
+	const copyColor = () => {
+		navigator.clipboard.writeText(color);
+	};
+
+	return (
+		<div className="App">
+			<div className="group_image">
+				<img
+					src={`https://picsum.photos/id/${number}/536/354`}
+					alt="src"
+				/>
+
+				<div className="color">
+					{color ? (
+						<button
+							className="button"
+							type="button"
+							onClick={copyColor}
+							style={{ backgroundColor: color }}
+						>
+							{color}
+						</button>
+					) : null}
+				</div>
+			</div>
+			<div className="group_button">
+				<button className="button" type="button" onClick={randomNumber}>
+					Random Image
+				</button>
+				<button className="button" type="button" onClick={pickColor}>
+					Pick Color
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
